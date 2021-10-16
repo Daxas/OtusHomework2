8e35f888ffe877b33cd175db7e2ac5417e0318e7
Daxas <40495637+Daxas@users.noreply.github.com>
2021-10-16 11:12:26 +0300

    Add files via upload

---
+++ OtusHomework2/OtusHomework2/OtusHomework2App.swift
@@ -0,0 +1,17 @@
+//
+//  OtusHomework2App.swift
+//  OtusHomework2
+//
+//  Created by allme on 16.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct OtusHomework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
